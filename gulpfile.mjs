a9b2c001cd196b360759cbe5ff734a0e17b7b405
Volker Scheuber <volker.scheuber@forgerock.com>
2022-08-20 15:09:30 -0500

    hybrid package.json and build tools

---
+++ gulpfile.mjs
@@ -0,0 +1,50 @@
+import gulp from 'gulp';
+import babel from 'gulp-babel';
+import del from 'del';
+import sourcemaps from 'gulp-sourcemaps';
+import map from 'map-stream';
+
+gulp.task('clean', () => del(['cjs']));
+
+gulp.task('package', () =>
+  gulp
+    .src('*.json')
+    .pipe(
+      map((file, done) => {
+        const json = JSON.parse(file.contents.toString());
+        delete json.type;
+        // eslint-disable-next-line no-param-reassign
+        file.contents = Buffer.from(JSON.stringify(json));
+        done(null, file);
+      })
+    )
+    .pipe(gulp.dest('cjs'))
+);
+
+gulp.task('transpile', () =>
+  gulp
+    .src(['src/*.mjs', 'src/**/*.mjs'])
+    .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(
+      babel({
+        plugins: [
+          ['@babel/plugin-proposal-export-namespace-from'],
+          [
+            '@babel/plugin-transform-modules-commonjs',
+            {
+              importInterop: 'babel',
+            },
+          ],
+          ['babel-plugin-transform-import-meta'],
+        ],
+      })
+    )
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('cjs'))
+);
+
+gulp.task('resources', () =>
+  gulp.src(['src/**/*.json'], { base: './src/' }).pipe(gulp.dest('cjs'))
+);
+
+gulp.task('default', gulp.series('clean', 'transpile', 'resources'));
