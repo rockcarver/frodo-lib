271cd6ab6b7366bcc5a556c1a77622fff96d6a0c
Volker Scheuber <volker.scheuber@forgerock.com>
2022-09-10 12:06:19 -0500

    progress on #76 - Create unit tests for JourneyOps.ts

---
+++ src/ops/JourneyOps.test.ts
@@ -0,0 +1,173 @@
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
+import { Journey, state } from '../index';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import * as global from '../storage/StaticStorage';
+import { assert } from 'console';
+
+const mock = new MockAdapter(axios);
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+state.default.session.setTenant('');
+state.default.session.setRealm('alpha');
+state.default.session.setCookieName('cookieName');
+state.default.session.setCookieValue('cookieValue');
+state.default.session.setDeploymentType(global.CLOUD_DEPLOYMENT_TYPE_KEY);
+
+describe('JourneyOps - getJourneys()', () => {
+  test('getJourneys() 0: Method is implemented', async () => {
+    expect(Journey.getJourneys).toBeDefined();
+  });
+
+  test('getJourneys() 1: Get all journeys', async () => {
+    const mockResponse = JSON.parse(
+      fs.readFileSync(
+        path.resolve(__dirname, '../../test/mocks/TreeApi/getTrees/trees.json'),
+        'utf8'
+      )
+    );
+    mock
+      .onGet(
+        '/json/realms/root/realms/alpha/realm-config/authentication/authenticationtrees/trees?_queryFilter=true'
+      )
+      .reply(200, mockResponse);
+    const journeys = await Journey.getJourneys();
+    expect(journeys).toBeTruthy();
+    expect(journeys.length).toBe(92);
+  });
+});
+
+describe('JourneyOps - exportJourney()', () => {
+  test('exportJourney() 0: Method is implemented', async () => {
+    expect(Journey.exportJourney).toBeDefined();
+  });
+
+  test('exportJourney() 1: Export single journey w/o dependencies', async () => {
+    const mockGetTreeResponse = JSON.parse(
+      fs.readFileSync(
+        path.resolve(
+          __dirname,
+          '../../test/mocks/TreeApi/getTree/FrodoTest.json'
+        ),
+        'utf8'
+      )
+    );
+    mock
+      .onGet(
+        '/json/realms/root/realms/alpha/realm-config/authentication/authenticationtrees/trees/FrodoTest'
+      )
+      .reply(200, mockGetTreeResponse);
+    mock
+      .onGet(
+        /\/json\/realms\/root\/realms\/alpha\/realm-config\/authentication\/authenticationtrees\/nodes\/.+/
+      )
+      .reply(function (config) {
+        const elements = config.url ? config.url.split('/') : [];
+        const nodeType = elements[elements?.length - 2];
+        const nodeId = elements[elements?.length - 1];
+        const mockStatus = 200;
+        const mockResponse = JSON.parse(
+          fs.readFileSync(
+            path.resolve(
+              __dirname,
+              `../../test/mocks/NodeApi/getNode/${nodeType}/${nodeId}.json`
+            ),
+            'utf8'
+          )
+        );
+        expect(mockResponse._id).toBe(nodeId);
+        return [mockStatus, mockResponse];
+      });
+    expect.assertions(23);
+    const journeyExport = await Journey.exportJourney('FrodoTest', {
+      useStringArrays: false,
+      deps: false,
+      verbose: false,
+    });
+    expect(journeyExport).toBeTruthy();
+    expect(journeyExport.tree['_id']).toBe('FrodoTest');
+    expect(journeyExport.tree).toStrictEqual(mockGetTreeResponse);
+    expect(Object.keys(journeyExport.nodes as object).length).toBe(7);
+    expect(Object.keys(journeyExport.innerNodes as object).length).toBe(5);
+    expect(Object.keys(journeyExport.circlesOfTrust as object).length).toBe(0);
+    expect(Object.keys(journeyExport.emailTemplates as object).length).toBe(0);
+    expect(Object.keys(journeyExport.saml2Entities as object).length).toBe(0);
+    expect(Object.keys(journeyExport.scripts as object).length).toBe(0);
+    expect(
+      Object.keys(journeyExport.socialIdentityProviders as object).length
+    ).toBe(0);
+    expect(journeyExport.themes?.length).toBe(0);
+  });
+
+  test.skip('exportJourney() 2: Export single journey w/ dependencies', async () => {
+    const mockGetTreeResponse = JSON.parse(
+      fs.readFileSync(
+        path.resolve(
+          __dirname,
+          '../../test/mocks/TreeApi/getTree/FrodoTest.json'
+        ),
+        'utf8'
+      )
+    );
+    mock
+      .onGet(
+        '/json/realms/root/realms/alpha/realm-config/authentication/authenticationtrees/trees/FrodoTest'
+      )
+      .reply(200, mockGetTreeResponse);
+    const mockGetThemesResponse = JSON.parse(
+      fs.readFileSync(
+        path.resolve(
+          __dirname,
+          '../../test/mocks/IdmConfigApi/getConfigEntity/themes.json'
+        ),
+        'utf8'
+      )
+    );
+    mock
+      .onGet('/openidm/config/ui/themerealm')
+      .reply(200, mockGetThemesResponse);
+
+    mock
+      .onGet(
+        /\/json\/realms\/root\/realms\/alpha\/realm-config\/authentication\/authenticationtrees\/nodes\/.+/
+      )
+      .reply(function (config) {
+        const elements = config.url ? config.url.split('/') : [];
+        const nodeType = elements[elements?.length - 2];
+        const nodeId = elements[elements?.length - 1];
+        const mockStatus = 200;
+        const mockResponse = JSON.parse(
+          fs.readFileSync(
+            path.resolve(
+              __dirname,
+              `../../test/mocks/NodeApi/getNode/${nodeType}/${nodeId}.json`
+            ),
+            'utf8'
+          )
+        );
+        expect(mockResponse._id).toBe(nodeId);
+        return [mockStatus, mockResponse];
+      });
+    expect.assertions(22);
+    const journeyExport = await Journey.exportJourney('FrodoTest', {
+      useStringArrays: false,
+      deps: false,
+      verbose: false,
+    });
+    expect(journeyExport).toBeTruthy();
+    expect(journeyExport.tree['_id']).toBe('FrodoTest');
+    expect(Object.keys(journeyExport.nodes as object).length).toBe(7);
+    expect(Object.keys(journeyExport.innerNodes as object).length).toBe(5);
+    expect(Object.keys(journeyExport.circlesOfTrust as object).length).toBe(0);
+    expect(Object.keys(journeyExport.emailTemplates as object).length).toBe(0);
+    expect(Object.keys(journeyExport.saml2Entities as object).length).toBe(0);
+    expect(Object.keys(journeyExport.scripts as object).length).toBe(0);
+    expect(
+      Object.keys(journeyExport.socialIdentityProviders as object).length
+    ).toBe(0);
+    expect(journeyExport.themes?.length).toBe(0);
+  });
+});
