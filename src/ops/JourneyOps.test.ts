844a991973b78d8f7616181ba9ff12db6fbe57b5
Volker Scheuber <volker.scheuber@forgerock.com>
2022-09-11 13:35:56 -0500

    progresses #76 - create unit tests for JourneyOps.ts

--- src/ops/JourneyOps.test.ts
+++ src/ops/JourneyOps.test.ts
@@ -3,8 +3,14 @@ import MockAdapter from 'axios-mock-adapter';
 import { Journey, state } from '../index';
 import * as global from '../storage/StaticStorage';
 import {
+  mockGetSocialIdentityProviders,
+  mockGetSaml2ProviderMetadata,
+  mockGetSaml2ProviderByLocationAndId,
+  mockGetSaml2Providers,
+  mockGetCirclesOfTrust,
+  mockGetConfigEntity,
+  mockGetScript,
   getTree,
-  getTrees,
   mockGetTrees,
   mockGetTree,
   mockGetNode,
@@ -12,7 +18,7 @@ import {
 
 const mock = new MockAdapter(axios);
 
-state.default.session.setTenant('');
+state.default.session.setTenant('https://openam-frodo-dev.forgeblocks.com/am');
 state.default.session.setRealm('alpha');
 state.default.session.setCookieName('cookieName');
 state.default.session.setCookieValue('cookieValue');
@@ -62,29 +68,37 @@ describe('JourneyOps - exportJourney()', () => {
     expect(journeyExport.themes?.length).toBe(0);
   });
 
-  test.skip('exportJourney() 2: Export single journey w/ dependencies', async () => {
+  test('exportJourney() 2: Export single journey w/ dependencies', async () => {
     mockGetTree(mock);
     mockGetNode(mock);
+    mockGetConfigEntity(mock);
+    mockGetScript(mock);
+    mockGetSaml2Providers(mock);
+    mockGetCirclesOfTrust(mock);
+    mockGetSaml2ProviderByLocationAndId(mock);
+    mockGetSaml2ProviderMetadata(mock);
+    mockGetSocialIdentityProviders(mock);
     const treeId = 'FrodoTest';
     const treeObject = getTree(treeId);
-    expect.assertions(22);
+    expect.assertions(42);
     const journeyExport = await Journey.exportJourney('FrodoTest', {
       useStringArrays: false,
       deps: true,
       verbose: false,
     });
+    // console.dir(journeyExport);
     expect(journeyExport).toBeTruthy();
     expect(journeyExport.tree['_id']).toBe(treeId);
     expect(journeyExport.tree).toStrictEqual(treeObject);
     expect(Object.keys(journeyExport.nodes as object).length).toBe(7);
     expect(Object.keys(journeyExport.innerNodes as object).length).toBe(5);
-    expect(Object.keys(journeyExport.circlesOfTrust as object).length).toBe(0);
-    expect(Object.keys(journeyExport.emailTemplates as object).length).toBe(0);
-    expect(Object.keys(journeyExport.saml2Entities as object).length).toBe(0);
-    expect(Object.keys(journeyExport.scripts as object).length).toBe(0);
+    expect(Object.keys(journeyExport.circlesOfTrust as object).length).toBe(1);
+    expect(Object.keys(journeyExport.emailTemplates as object).length).toBe(1);
+    expect(Object.keys(journeyExport.saml2Entities as object).length).toBe(2);
+    expect(Object.keys(journeyExport.scripts as object).length).toBe(9);
     expect(
       Object.keys(journeyExport.socialIdentityProviders as object).length
-    ).toBe(0);
-    expect(journeyExport.themes?.length).toBe(0);
+    ).toBe(8);
+    expect(journeyExport.themes?.length).toBe(1);
   });
 });
