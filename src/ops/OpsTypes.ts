c9a6962c83bcd99841a3a6981e900e25ca9dbe11
Volker Scheuber <volker.scheuber@forgerock.com>
2022-09-30 08:38:11 -0500

    Typification

---
+++ src/ops/OpsTypes.ts
@@ -0,0 +1,82 @@
+import {
+  CircleOfTrustSkeleton,
+  EmailTemplateSkeleton,
+  NodeSkeleton,
+  Saml2ProviderSkeleton,
+  ScriptSkeleton,
+  SocialIdpSkeleton,
+  ThemeSkeleton,
+  TreeSkeleton,
+} from '../api/ApiTypes';
+
+/**
+ * Tree export options
+ */
+export interface TreeExportOptions {
+  /**
+   * Where applicable, use string arrays to store multi-line text (e.g. scripts).
+   */
+  useStringArrays: boolean;
+  /**
+   * Include any dependencies (scripts, email templates, SAML entity providers and circles of trust, social identity providers, themes).
+   */
+  deps: boolean;
+  /**
+   * Verbose output during command execution. May or may not produce additional output.
+   */
+  verbose: boolean;
+}
+
+/**
+ * Tree import options
+ */
+export interface TreeImportOptions {
+  /**
+   * Generate new UUIDs for all nodes during import.
+   */
+  reUuid: boolean;
+  /**
+   * Include any dependencies (scripts, email templates, SAML entity providers and circles of trust, social identity providers, themes).
+   */
+  deps: boolean;
+  /**
+   * Verbose output during command execution. May or may not produce additional output.
+   */
+  verbose: boolean;
+}
+
+export interface ExportMetaData {
+  origin: string;
+  originAmVersion: string;
+  exportedBy: string;
+  exportDate: string;
+  exportTool: string;
+  exportToolVersion: string;
+}
+
+export interface SingleTreeExportInterface {
+  meta?: Record<string, ExportMetaData>;
+  innerNodes?: Record<string, NodeSkeleton>;
+  innernodes?: Record<string, NodeSkeleton>;
+  nodes: Record<string, NodeSkeleton>;
+  scripts: Record<string, ScriptSkeleton>;
+  emailTemplates: Record<string, EmailTemplateSkeleton>;
+  socialIdentityProviders: Record<string, SocialIdpSkeleton>;
+  themes: ThemeSkeleton[];
+  saml2Entities: Record<string, Saml2ProviderSkeleton>;
+  circlesOfTrust: Record<string, CircleOfTrustSkeleton>;
+  tree: TreeSkeleton;
+}
+
+export interface MultiTreeExportInterface {
+  meta?: Record<string, ExportMetaData>;
+  trees: Record<string, SingleTreeExportInterface>;
+}
+
+export interface TreeDependencyMapInterface {
+  [k: string]: TreeDependencyMapInterface[];
+}
+
+export interface TreeExportResolverInterface {
+  (treeId: string): Promise<SingleTreeExportInterface>;
+}
