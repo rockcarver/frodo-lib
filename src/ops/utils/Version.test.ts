72eb758c28d88a344a35d0886ba4ee8e3704893b
Sandeep Chaturvedi <schaturvedi@gmail.com>
2022-10-11 15:45:35 -0600

    fixed lint error

--- src/ops/utils/Version.test.ts
+++ src/ops/utils/Version.test.ts
@@ -6,7 +6,7 @@ import { fileURLToPath } from 'url';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const pkg = JSON.parse(
-    fs.readFileSync(path.resolve(__dirname, '../../../package.json'), 'utf8')
+  fs.readFileSync(path.resolve(__dirname, '../../../package.json'), 'utf8')
 );
 
 describe('index', () => {
