e7d2dc5f8f773398dc90ba0d2937a1f2c0e16eca
Sandeep Chaturvedi <schaturvedi@gmail.com>
2022-10-11 15:29:04 -0600

    refactored update check logic

--- src/ops/utils/Version.ts
+++ src/ops/utils/Version.ts
@@ -1,50 +1,24 @@
 import fs from 'fs';
-import axios, { AxiosProxyConfig } from 'axios';
 import path from 'path';
+import { generateReleaseApi } from '../../api/BaseApi';
 
 import { fileURLToPath } from 'url';
-
-const timeout = 30000;
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const pkg = JSON.parse(
   fs.readFileSync(path.resolve(__dirname, '../../../package.json'), 'utf8')
 );
-const userAgent = `${pkg.name}/${pkg.version}`;
+// const userAgent = `${pkg.name}/${pkg.version}`;
 
 export function getVersion() {
   return `${pkg.version}`;
 }
 
-function getProxy(): AxiosProxyConfig | false {
-  if (process.env.HTTPS_PROXY || process.env.https_proxy) return false;
-  return null;
-}
-
-function generateApi(baseURL, requestOverride = {}) {
-  const requestDetails = {
-    baseURL: baseURL,
-    timeout,
-    headers: {
-      'User-Agent': userAgent,
-      'Content-Type': 'application/json',
-    },
-    ...requestOverride,
-    proxy: getProxy(),
-  };
-
-  const request = axios.create(requestDetails);
-
-  return request;
-}
-
 export async function getAllVersions(endpoints) {
-  //   const versionInfo = [];
   const reqPromises = [];
   endpoints.forEach((item) => {
-    // reqPromises.push(item);
-    reqPromises.push(generateApi(item.base).get(item.path));
+    reqPromises.push(generateReleaseApi(item.base).get(item.path));
   });
   const result = await Promise.allSettled(reqPromises);
   return result;
