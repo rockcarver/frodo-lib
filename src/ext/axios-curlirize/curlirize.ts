aef3bac43db90c93c70eb48bcb7d76c6e7b8078d
Volker Scheuber <volker.scheuber@forgerock.com>
2022-11-09 15:08:56 -0600

    adopt/internalize axios-curlirize to enable transpilation to CJS for binary builds

---
+++ src/ext/axios-curlirize/curlirize.ts
@@ -0,0 +1,37 @@
+import { CurlHelper } from './lib/CurlHelper';
+
+function defaultLogCallback(curlResult, err = undefined) {
+  const { command } = curlResult;
+  if (err) {
+    console.error(err);
+  } else {
+    console.info(command);
+  }
+}
+
+export default (instance, callback = defaultLogCallback) => {
+  instance.interceptors.request.use((req) => {
+    try {
+      const curl = new CurlHelper(req);
+      req.curlObject = curl;
+      req.curlCommand = curl.generateCommand();
+      req.clearCurl = () => {
+        delete req.curlObject;
+        delete req.curlCommand;
+        delete req.clearCurl;
+      };
+    } catch (err) {
+      // Even if the axios middleware is stopped, no error should occur outside.
+      callback(null, err);
+    } finally {
+      if (req.curlirize !== false) {
+        callback({
+          command: req.curlCommand,
+          object: req.curlObject,
+        });
+      }
+      // eslint-disable-next-line no-unsafe-finally
+      return req;
+    }
+  });
+};
