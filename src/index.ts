8958fd62fa5fe6dedcf1ce87ef3743224936452b
Sandeep Chaturvedi <schaturvedi@gmail.com>
2022-11-25 08:44:40 -0700

    - Refactored Idm Config API, moved file operations to cli - Added unit tests

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,7 @@ export * as StartupRaw from './api/StartupApi';
 export * as TreeRaw from './api/TreeApi';
 export * as TypesRaw from './api/ApiTypes';
 export * as VariablesRaw from './api/VariablesApi';
+export * as IdmConfigRaw from './api/IdmConfigApi';
 
 // Ops Layer
 export * as Admin from './ops/AdminOps';
@@ -18,8 +19,8 @@ export * as Authenticate from './ops/AuthenticateOps';
 export * as CirclesOfTrust from './ops/CirclesOfTrustOps';
 export * as ConnectionProfile from './ops/ConnectionProfileOps';
 export * as EmailTemplate from './ops/EmailTemplateOps';
-export * as Idm from './ops/IdmOps';
 export * as Idp from './ops/IdpOps';
+export * as Idm from './ops/IdmOps';
 export * as Journey from './ops/JourneyOps';
 export * as Log from './ops/LogOps';
 export * as ManagedObject from './ops/ManagedObjectOps';
@@ -37,6 +38,7 @@ export * as Types from './ops/OpsTypes';
 export * as Variables from './ops/VariablesOps';
 // TODO: revisit if there are better ways
 export * as Utils from './ops/utils/OpsUtils';
+export * as ValidationUtils from './ops/utils/ValidationUtils';
 export * as LibVersion from './ops/utils/Version';
 export * as ExportImportUtils from './ops/utils/ExportImportUtils';
 // TODO: reconsider the aproach to pass in state from client
