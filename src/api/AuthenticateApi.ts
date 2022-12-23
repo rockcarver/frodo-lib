42d1e7f8a40baec3a7dd86dc31b8ed51044c9594
Volker Scheuber <volker.scheuber@forgerock.com>
2022-12-23 17:47:45 -0600

    migrate AuthenticateApi tests to Polly

--- src/api/AuthenticateApi.ts
+++ src/api/AuthenticateApi.ts
@@ -11,15 +11,37 @@ const getApiConfig = () => ({
   apiVersion,
 });
 
-export async function step(body = {}, config = {}) {
+/**
+ * Fill callbacks from a map
+ * Just a start
+ * @param {object} response json response from a call to /authenticate
+ * @param {{ [k: string]: string | number | boolean | string[] }} map name/value map
+ * @returns filled response body so it can be used as input to another call to /authenticate
+ */
+export function fillCallbacks(
+  response: object,
+  map: { [k: string]: string | number | boolean | string[] }
+): object {
+  const body = JSON.parse(JSON.stringify(response));
+  for (const callback of body.callbacks) {
+    callback.input[0].value = map[callback.input[0].name];
+  }
+  return body;
+}
+
+export async function step(body = {}, config = {}, realm = '/') {
   const urlString = state.getAuthenticationService()
     ? util.format(
         authenticateWithServiceUrlTemplate,
         state.getHost(),
-        getRealmPath('/'),
+        getRealmPath(realm),
         state.getAuthenticationService()
       )
-    : util.format(authenticateUrlTemplate, state.getHost(), getRealmPath('/'));
+    : util.format(
+        authenticateUrlTemplate,
+        state.getHost(),
+        getRealmPath(realm)
+      );
   const { data } = await generateAmApi(getApiConfig()).post(
     urlString,
     body,
