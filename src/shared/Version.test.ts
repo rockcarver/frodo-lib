e2de573a399ab7b8402c6b9eb22e32be9eedab35
Huston Franklin <hfranklin@users.noreply.github.com>
2023-05-14 21:16:47 -0600

    Create TriVir release

--- src/shared/Version.test.ts
+++ src/shared/Version.test.ts
@@ -2,6 +2,6 @@ import { getUserAgent } from './Version';
 
 describe('Versions', () => {
     test('user agent is compiled from package', () => {
-        expect(getUserAgent()).toMatch(/@rockcarver\/frodo-lib\/[0-9.-]+/);
+        expect(getUserAgent()).toMatch(/@trivir\/frodo-lib\/[0-9.-]+trivir[0-9.]*/);
     });
 });
