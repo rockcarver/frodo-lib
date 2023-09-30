8b129a183b1f0a062a91094d1669ce87d9d9f534
Volker Scheuber <volker.scheuber@forgerock.com>
2023-09-29 22:42:08 -0500

    add method to filter recordings

--- src/utils/AutoSetupPolly.ts
+++ src/utils/AutoSetupPolly.ts
@@ -9,8 +9,10 @@ import { fileURLToPath } from 'url';
 
 import { state } from '../index';
 import { getTokens } from '../ops/AuthenticateOps';
+import { encode, isBase64Encoded } from './Base64Utils';
 
 const { setupPolly } = pollyJest;
+
 Polly.register(NodeHttpAdapter);
 Polly.register(FSPersister);
 
@@ -57,25 +59,76 @@ switch (process.env.FRODO_POLLY_MODE) {
     break;
 }
 
-export function defaultMatchRequestsBy() {
-  return JSON.parse(
-    JSON.stringify({
-      method: true,
-      headers: false, // do not match headers, because "Authorization" header is sent only at recording time
-      body: true,
-      order: false,
-      url: {
-        protocol: true,
-        username: false,
-        password: false,
-        hostname: false, // we will record from different envs but run tests always against `frodo-dev`
-        port: false,
-        pathname: true,
-        query: true,
-        hash: true,
-      },
-    })
-  );
+function defaultMatchRequestsBy() {
+  return {
+    method: true,
+    headers: false, // do not match headers, because "Authorization" header is sent only at recording time
+    body: true,
+    order: false,
+    url: {
+      protocol: true,
+      username: false,
+      password: false,
+      hostname: false, // we will record from different envs but run tests always against `frodo-dev`
+      port: false,
+      pathname: true,
+      query: true,
+      hash: true,
+    },
+  };
+}
+
+export function filterRecording(recording: {
+  request: {
+    headers: [{ name: string; value: string }];
+    postData: { text: any };
+  };
+  response: { content: { text: any } };
+}) {
+  // request headers
+  if (recording.request?.headers) {
+    const headers: [{ name: string; value: string }] =
+      recording.request.headers;
+    headers.map((header) => {
+      if (header.name.toUpperCase() === 'AUTHORIZATION') {
+        if (isBase64Encoded(header.value)) {
+          header.value = encode('username:password');
+        } else {
+          header.value = header.value.replace(
+            /Bearer .+/,
+            'Bearer <bearer token>'
+          );
+        }
+      }
+      if (header.name.toUpperCase() === 'X-API-KEY') {
+        header.value = '<api key>';
+      }
+      if (header.name.toUpperCase() === 'X-API-SECRET') {
+        header.value = '<api secret>';
+      }
+    });
+    recording.request.headers = headers;
+  }
+
+  // request post body
+  if (recording.request?.postData?.text) {
+    let body = recording.request.postData.text;
+    body = body.replace(/assertion=.+?&/, 'assertion=<assertion jwt token>&');
+    recording.request.postData.text = body;
+  }
+
+  // response body
+  if (recording.response?.content?.text) {
+    let body = recording.response.content.text;
+    try {
+      const json = JSON.parse(body);
+      if (json['access_token']) json['access_token'] = '<access token>';
+      body = JSON.stringify(json);
+    } catch (error) {
+      // ignore
+    }
+    recording.response.content.text = body;
+  }
 }
 
 export function autoSetupPolly(matchRequestsBy = defaultMatchRequestsBy()) {
