48b1ff3ba29f84cfc0d48c70529de7a719527b90
Volker Scheuber <volker.scheuber@forgerock.com>
2023-10-02 16:27:25 -0500

    Merge MiscUtils into ExportImportUtils

--- src/utils/ExportImportUtils.ts
+++ src/utils/ExportImportUtils.ts
@@ -70,6 +70,40 @@ export type ExportImport = {
   substituteEnvParams(input: string, reader: Reader): string;
 
   unSubstituteEnvParams(input: string, reader: Reader): string;
+  /*
+   * Parse a URL into its components and make them easily accessible by name
+   *
+   * Use in a Scripte Decision Node Script as follows:
+   * var referer = parseUrl(requestHeaders.get("referer").get(0));
+   * var origin = referer.origin;
+   *
+   * e.g.: https://openam-volker-dev.forgeblocks.com/am/XUI/?realm=/bravo&authIndexType=service&authIndexValue=InitiateOwnerClaim#/
+   * {
+   *     hash: '#/',
+   *     host: 'openam-volker-dev.forgeblocks.com',
+   *     hostname: 'openam-volker-dev.forgeblocks.com',
+   *     href: 'https://openam-volker-dev.forgeblocks.com/am/XUI/?realm=/bravo&authIndexType=service&authIndexValue=InitiateOwnerClaim#/',
+   *     origin: 'https://openam-volker-dev.forgeblocks.com',
+   *     pathname: '/am/XUI/',
+   *     port: '',
+   *     protocol: 'https',
+   *     search: '?realm=/bravo&authIndexType=service&authIndexValue=InitiateOwnerClaim',
+   *     username: '',
+   *     password: '',
+   *     searchParam: {
+   *         realm: '/bravo',
+   *         authIndexType: 'service',
+   *         authIndexValue: 'InitiateOwnerClaim'
+   *     }
+   * }
+   */
+  parseUrl(href: string): any;
+  /**
+   * Check if a string is a valid URL
+   * @param {string} urlString input string to be evaluated
+   * @returns {boolean} true if a valid URL, false otherwise
+   */
+  isValidUrl(urlString: string): boolean;
 };
 
 export default (state: State): ExportImport => {
@@ -139,6 +173,12 @@ export default (state: State): ExportImport => {
     },
     unSubstituteEnvParams(input: string, reader: Reader): string {
       return unSubstituteEnvParams(input, reader);
+    },
+    parseUrl(href: string): any {
+      return parseUrl(href);
+    },
+    isValidUrl(urlString: string): boolean {
+      return isValidUrl(urlString);
     },
   };
 };
@@ -378,4 +418,80 @@ export function unSubstituteEnvParams(input: string, reader: Reader): string {
     input = replaceall(`\${${key}}`, value, input);
   });
   return input;
+}
+
+/*
+ * Parse a URL into its components and make them easily accessible by name
+ *
+ * Use in a Scripte Decision Node Script as follows:
+ * var referer = parseUrl(requestHeaders.get("referer").get(0));
+ * var origin = referer.origin;
+ *
+ * e.g.: https://openam-volker-dev.forgeblocks.com/am/XUI/?realm=/bravo&authIndexType=service&authIndexValue=InitiateOwnerClaim#/
+ * {
+ *     hash: '#/',
+ *     host: 'openam-volker-dev.forgeblocks.com',
+ *     hostname: 'openam-volker-dev.forgeblocks.com',
+ *     href: 'https://openam-volker-dev.forgeblocks.com/am/XUI/?realm=/bravo&authIndexType=service&authIndexValue=InitiateOwnerClaim#/',
+ *     origin: 'https://openam-volker-dev.forgeblocks.com',
+ *     pathname: '/am/XUI/',
+ *     port: '',
+ *     protocol: 'https',
+ *     search: '?realm=/bravo&authIndexType=service&authIndexValue=InitiateOwnerClaim',
+ *     username: '',
+ *     password: '',
+ *     searchParam: {
+ *         realm: '/bravo',
+ *         authIndexType: 'service',
+ *         authIndexValue: 'InitiateOwnerClaim'
+ *     }
+ * }
+ */
+export function parseUrl(href: string): any {
+  const m = href.match(
+      /^(([^:/?#]+):?(?:\/\/((?:([^/?#:]*):([^/?#:]*)@)?([^/?#:]*)(?::([^/?#:]*))?)))?([^?#]*)(\?[^#]*)?(#.*)?$/
+    ),
+    r = {
+      hash: m[10] || '',
+      host: m[3] || '',
+      hostname: m[6] || '',
+      href: m[0] || '',
+      origin: m[1] || '',
+      pathname: m[8] || (m[1] ? '/' : ''),
+      port: m[7] || '',
+      protocol: m[2] || '',
+      search: m[9] || '',
+      username: m[4] || '',
+      password: m[5] || '',
+      searchParam: {}, // { realm: '/bravo',
+      //   authIndexType: 'service',
+      //   authIndexValue: 'InitiateOwnerClaim' }
+    };
+  if (r.protocol.length == 2) {
+    r.protocol = 'file:///' + r.protocol.toUpperCase();
+    r.origin = r.protocol + '//' + r.host;
+  }
+  if (r.search.length > 2) {
+    const query = r.search.indexOf('?') === 0 ? r.search.substr(1) : r.search;
+    const vars = query.split('&');
+    for (let i = 0; i < vars.length; i++) {
+      const pair = vars[i].split('=');
+      r.searchParam[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1]);
+    }
+  }
+  r.href = r.origin + r.pathname + r.search + r.hash;
+  return r;
+}
+
+/**
+ * Check if a string is a valid URL
+ * @param {string} urlString input string to be evaluated
+ * @returns {boolean} true if a valid URL, false otherwise
+ */
+export function isValidUrl(urlString: string): boolean {
+  try {
+    return Boolean(new URL(urlString));
+  } catch (error) {
+    return false;
+  }
 }
