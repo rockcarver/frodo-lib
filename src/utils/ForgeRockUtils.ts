ae79d49a53d7be2b7d6ba6d7670fe2c46c9d8b4e
Mike Mercer <mmercer@trivir.com>
2024-09-30 13:02:15 -0600

    add in get realm name

--- src/utils/ForgeRockUtils.ts
+++ src/utils/ForgeRockUtils.ts
@@ -9,6 +9,7 @@ export type FRUtils = {
   getCurrentRealmName(): string;
   getCurrentRealmManagedUser(): string;
   getRealmName(realm: string): string;
+  getRealmUsingExportFormat(realm: string): string;
   /**
    * Gets the list of realms to be used for exports in special format.
    * e.g. if the realm is normally '/first/second', then it will return 'root-first-second'.
@@ -79,6 +80,9 @@ export default (state: State): FRUtils => {
     },
     getIdmBaseUrl(): string {
       return getIdmBaseUrl(state);
+    },
+    getRealmUsingExportFormat(realm: string): string {
+      return getRealmUsingExportFormat(realm);
     },
 
     // deprecated
