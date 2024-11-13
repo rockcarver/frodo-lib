9ac5148f953fa1838c0590c14da3ced4d2b23d4b
Preston Hales <phales@trivir.com>
2024-11-13 09:17:00 -0700

    Add server imports and exports

--- src/lib/FrodoLib.ts
+++ src/lib/FrodoLib.ts
@@ -8,6 +8,7 @@ import AuthenticationSettingsOps, {
   AuthenticationSettings,
 } from '../ops/AuthenticationSettingsOps';
 import CirclesOfTrustOps, { CirclesOfTrust } from '../ops/CirclesOfTrustOps';
+import ServerOps, { Server } from '../ops/classic/ServerOps';
 import AdminFederationOps, {
   AdminFederation,
 } from '../ops/cloud/AdminFederationOps';
@@ -174,6 +175,7 @@ export type Frodo = {
   };
 
   script: Script;
+  server: Server;
   service: Service;
   session: Session;
 
@@ -332,6 +334,7 @@ const FrodoLib = (config: StateInterface = {}): Frodo => {
     },
 
     script: ScriptOps(state),
+    server: ServerOps(state),
     service: ServiceOps(state),
     session: SessionOps(state),
 
