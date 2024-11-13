f9c4a170bb93df3693580f77783171e7b972dee5
Preston Hales <phales@trivir.com>
2024-11-13 09:28:30 -0700

    Add secret store imports and exports

--- src/lib/FrodoLib.ts
+++ src/lib/FrodoLib.ts
@@ -9,6 +9,7 @@ import AuthenticationSettingsOps, {
   AuthenticationSettings,
 } from '../ops/AuthenticationSettingsOps';
 import CirclesOfTrustOps, { CirclesOfTrust } from '../ops/CirclesOfTrustOps';
+import SecretStoreOps, { SecretStore } from '../ops/classic/SecretStoreOps';
 import ServerOps, { Server } from '../ops/classic/ServerOps';
 import AdminFederationOps, {
   AdminFederation,
@@ -186,6 +187,7 @@ export type Frodo = {
 
   script: Script;
   server: Server;
+  secretStore: SecretStore;
   service: Service;
   session: Session;
 
@@ -354,6 +356,7 @@ const FrodoLib = (config: StateInterface = {}): Frodo => {
 
     script: ScriptOps(state),
     server: ServerOps(state),
+    secretStore: SecretStoreOps(state),
     service: ServiceOps(state),
     session: SessionOps(state),
 
