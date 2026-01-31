1e5ed1bf3c05c6d76d4ed37102b249097343c820
Volker Scheuber <vscheuber@gmail.com>
2026-01-31 14:16:46 -0700

    update jest to v30 and add custom jest exception reporter (#529)
    * Add additional debug output to getTokens()
    
    * update jest to v30 and add custom jest exception reporter

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,10 @@ module.exports = {
   snapshotResolver: '<rootDir>/snapshotResolve.js',
   testMatch: ['**/?(*.)(test).ts'],
   testTimeout: 120000,
+  reporters: [
+    'default',
+    ['<rootDir>/jest-exception-reporter.mjs', {}]
+  ],
   extensionsToTreatAsEsm: ['.ts'],
   transform: {
     '^.+\\.tsx?$': ['ts-jest', { useESM: true }]
